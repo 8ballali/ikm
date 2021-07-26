df57a76c8c6af36c4b42465b241a8cd437ced335
Bagu <bagubragowo3@gmail.com>
2021-07-26 16:19:28 +0700

    saaa

--- resources/views/user/thankyou.blade.php
+++ resources/views/user/thankyou.blade.php
@@ -35,7 +35,7 @@
             e.preventDefault(); //will stop the link href to call the blog page
 
             setTimeout(function () {
-                window.location.href = "{{utl('/')}}"; //will redirect to your blog page (an ex: blog.html)
+                window.location.href = "{{url('/')}}"; //will redirect to your blog page (an ex: blog.html)
                 }, 3000); //will call the function after 2 secs.
         });
     </script>
