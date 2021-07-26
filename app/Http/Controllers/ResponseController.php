11c8d0d3a6a5d97fa17432705befc9aea19d099c
Bagu <bagubragowo3@gmail.com>
2021-07-26 15:39:39 +0700

    sa

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -15,6 +15,6 @@ class ResponseController extends Controller
             'answer_id' => $request->answer_id
         ]);
         echo "<script>setTimeout(function(){ window.location.href = 'https://ikm.idaman.org/penilaian/response'; }, 30000);</script>";
-        return view('user.thankyou');
+        return view('user.index');
     }
 }
