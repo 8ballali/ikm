c4d56869c1ddcc1d406bf00fb97ae9557a68b05a
Bagu <bagubragowo3@gmail.com>
2021-07-26 16:17:41 +0700

    saaa

--- resources/views/user/thankyou.blade.php
+++ resources/views/user/thankyou.blade.php
@@ -30,7 +30,15 @@
     </div>
 
 
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) { 
+            e.preventDefault(); //will stop the link href to call the blog page
 
+            setTimeout(function () {
+                window.location.href = "{{utl('/')}}"; //will redirect to your blog page (an ex: blog.html)
+                }, 3000); //will call the function after 2 secs.
+        });
+    </script>
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Source+Sans+Pro" rel="stylesheet">
 </body>
 
